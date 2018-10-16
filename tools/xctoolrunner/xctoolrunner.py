0041902688130a6709e820c646c74fd55281dcb9
zhangxinzheng <zhangxzheng@gmail.com>
2018-10-16 14:21:51 +0800

    fix for experimental_new_directory

--- tools/xctoolrunner/xctoolrunner.py
+++ tools/xctoolrunner/xctoolrunner.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##
+#
 """Wrapper for "xcrun" tools.
 
 This script only runs on Darwin and you must have Xcode installed.
